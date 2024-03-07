5c1a5291f75e382704861c5d4ce21368c5134b29
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-06 18:49:15 -0700

    initial setup database

---
+++ frontend/lib/database/models/reservation.model.ts
@@ -0,0 +1,52 @@
+import { Schema, model, models, Document } from 'mongoose'
+
+export interface IReservation extends Document {
+  createdAt: Date
+  stripeId: string
+  totalAmount: string
+  service: {
+    _id: string
+    title: string
+  }
+  buyer: {
+    _id: string
+    firstName: string
+    lastName: string
+  }
+}
+
+export type IReservationItem = {
+  _id: string
+  totalAmount: string
+  createdAt: Date
+  eventTitle: string
+  eventId: string
+  buyer: string
+}
+
+const ReservationSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  totalAmount: {
+    type: String,
+  },
+  service: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event',
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+})
+
+const Reservation = models.Reservation || model('Reservation', ReservationSchema)
+
+export default Reservation
---
+++ frontend/lib/database/models/service.model.ts
@@ -0,0 +1,36 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IService extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string, name: string }
+  provider: { _id: string, firstName: string, lastName: string }
+}
+
+const ServiceSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  provider: { type: Schema.Types.ObjectId, ref: 'User' },
+})
+
+const Service = models.Service || model('Service', ServiceSchema);
+
+export default Service;
