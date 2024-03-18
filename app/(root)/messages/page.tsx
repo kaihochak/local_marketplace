98cc9d825a9b2a5875ccf6385b45b051fd19b498
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-18 00:58:47 -0600

    Update page.tsx

--- app/(root)/messages/page.tsx
+++ app/(root)/messages/page.tsx
@@ -1,8 +1,6 @@
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import dummyData from '@/constants/dummydata'; // Adjust the import path according to your project structure
 
 const page = () => {
-    const { users, categories, services, ratingReviews, reservations } = dummyData;
 
     return (
         <div className="px-4">
