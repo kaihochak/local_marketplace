93b967889772ad27f9e95cd77e32c8d599510bc5
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-19 19:36:50 -0600

    user schema update

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -23,9 +23,9 @@ const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   imageURL: { type: String, required: true },
-  website: { type: String, required: true },
-  location: { type: String, required: true },
-  contactNumber: { type: String, required: true, unique: true },
+  website: { type: String},
+  location: { type: String},
+  contactNumber: { type: String, unique: true },
   serviceIDs: [{ type: Schema.Types.ObjectId, ref: 'Service' }],
   ratingReviewIDs: [{ type: Schema.Types.ObjectId, ref: 'RatingReview' }],
 }, { timestamps: true }); // Automatically add createdAt and updatedAt fields
