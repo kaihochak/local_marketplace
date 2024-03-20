96f59af40e58ae909e1fee8ebdd986eaf6df10b5
kusumpreetheer <55485344+kusumpreetheer@users.noreply.github.com>
2024-03-20 00:34:14 -0600

    nextprevbutton

--- constants/dummyServices.ts
+++ constants/dummyServices.ts
@@ -88,6 +88,94 @@ export const dummyServices: ServiceItem[] = [
         },
         ratingReviewIDs: ["1", "2", "3"],
     },
+    {
+        _id: "3",
+        title: "Ass Painter",
+        description: "Hey boiii, I'm a wall painter and I paint asses on walls",
+        image: [
+            "https://picsum.photos/seed/picsum/200/300",
+            "https://picsum.photos/seed/picsum/200/300",
+            "https://picsum.photos/seed/picsum/200/300",
+        ],
+        averageRating: 3.2,
+        totalReviews: 12,
+        serviceProvider: [
+            {
+                userId: "1", 
+                name: "Kanye West", 
+                imageURL: "https://picsum.photos/seed/picsum/200/300"
+            },
+            {
+                userId: "2", 
+                name: "Kim Kardashian", 
+                imageURL: "https://picsum.photos/seed/picsum/200/300"
+            }
+        ],
+        servicesOffered: {
+            service1: {
+                title: "Painting all walls",
+                price: "100",
+            },
+            service2: {
+                title: "Mural",
+                price: "200",
+            },
+            service3: {
+                title: "Painting a single wall",
+                price: "300",
+            },
+            service4: {
+                title: "Paint somewhere else hehe 🌗",
+                price: "400",
+            },
+        },
+        ratingReviewIDs: ["1", "2", "3"],
+
+    },
+    {
+        _id: "4",
+        title: "Me Painter",
+        description: "Hey boiii, I'm a wall painter and I paint asses on walls",
+        image: [
+            "https://picsum.photos/seed/picsum/200/300",
+            "https://picsum.photos/seed/picsum/200/300",
+            "https://picsum.photos/seed/picsum/200/300",
+        ],
+        averageRating: 3.2,
+        totalReviews: 12,
+        serviceProvider: [
+            {
+                userId: "1", 
+                name: "Kanye West", 
+                imageURL: "https://picsum.photos/seed/picsum/200/300"
+            },
+            {
+                userId: "2", 
+                name: "Kim Kardashian", 
+                imageURL: "https://picsum.photos/seed/picsum/200/300"
+            }
+        ],
+        servicesOffered: {
+            service1: {
+                title: "Painting all walls",
+                price: "100",
+            },
+            service2: {
+                title: "Mural",
+                price: "200",
+            },
+            service3: {
+                title: "Painting a single wall",
+                price: "300",
+            },
+            service4: {
+                title: "Paint somewhere else hehe 🌗",
+                price: "400",
+            },
+        },
+        ratingReviewIDs: ["1", "2", "3"],
+
+    }
 ];
 
 export default dummyServices; // Export 'dummyServices' as the default export
