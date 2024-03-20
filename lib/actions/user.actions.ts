28f5786960df5aa0e0d83fd2fd17fa94add08a4c
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-19 20:48:39 -0600

    testing

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -19,7 +19,6 @@ export async function createUser(user: CreateUserParams) {
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
     console.log('user.actions.ts: createUser - ', error);
-    
     handleError(error)
   }
 }
