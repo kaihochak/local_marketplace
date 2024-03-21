da761a9cdf07f2266c03e7d4dc1f65a9db3a27a3
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-20 18:26:27 -0600

    Users done

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,66 +1,36 @@
 import { createUser, deleteUser } from '@/lib/actions/user.actions';
 import { connectToDatabase } from '@/lib/database';
 import User from '@/lib/database/models/user.model';
-// import { dummyUsers } from '@/constants/dummyUsers';
+import { dummyUsers } from '@/constants/dummyUsersSeeding';
+import { dummyServices } from '@/constants/dummyServicesSeeding';
+
 import { NextResponse } from 'next/server';
-
-const dummyUsers = [
-    {
-        clerkId: "clerkId1",
-        imageURL: "https://picsum.photos/id/237/200/300",
-        email: "chak@yahoo.com",
-        website: "enyaumanzor.com",
-        username: "enyaumanzor",
-        firstName: "Enya",
-        lastName: "Umanzor",
-        location: "24 whitecres Ave, Calgary AB",
-        contactNumber: "587 946 5892",
-        serviceIDs: ["1", "2"],
-        ratingReviewIDs: ["1", "2"],
-    },
-];
-
-const dummyUser = {
-    clerkId: "clerkId1",
-    email: "chak@yahoo.com",
-    username: "enyaumanzor",
-    firstName: "Enya",
-    lastName: "Umanzor",
-    imageUrl: "https://picsum.photos/id/237/200/300",
-};
 
 export async function POST(req: Request) {
 
     console.log('Seeding database');
 
     // delete all users
-    deleteAllUsers();
+    await deleteAllUsers();
 
     // create users
-    createAllUsers(dummyUser);    
-
-    // let newUsers: typeof User[] = [];
-
-    // // seed users
-    // dummyUsers.forEach(async (user) => {
-    //     const newUser = await User.create(user);
-    //     if (!newUser) {
-    //         throw new Error('Failed to create user');
-    //     }
-    //     console.log('Created user', newUser);
-    //     newUsers.push(newUser);
-    // });
+    await createAllUsers(dummyUsers);    
 
     return NextResponse.json({ message: 'OK' });
-
 }
 
-const createAllUsers = async (dummyUser: any) => {
-    const newUser = await createUser(dummyUser);
-    if(newUser) {
-        console.log('Created user', newUser);
+const createAllUsers = async (dummyUsers: any[]) => {
+
+    console.log('Creating all users');
+
+    // create users
+    for (let i = 0; i < dummyUsers.length; i++) {
+        const user = dummyUsers[i];
+        await createUser(user);
     }
-    return NextResponse.json({ message: 'OK', user: newUser })
+
+    
+    return NextResponse.json({ message: 'OK', user: dummyUsers });
 }
 
 const deleteAllUsers = async () => {
