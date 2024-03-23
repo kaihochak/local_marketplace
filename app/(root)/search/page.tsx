399b155558009da70320f7652bfb30f9a809e4c5
kusumpreetheer <55485344+kusumpreetheer@users.noreply.github.com>
2024-03-23 17:06:34 -0600

    filter (not done)

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,8 +1,41 @@
+import { Filter } from '@/public/assets/icons/Filter'
+import { SearchIcon } from '@/public/assets/icons/SearchIcon'
 import React from 'react'
+import Collection from '@/components/shared/Collection'
+import { dummyServices } from '@/constants/dummyServices'
+import FilterButton from '@/components/shared/FilterButton'
+
 
 const page = () => {
+
   return (
-    <div>page</div>
+    <>
+
+      {/* Search & Filter */}
+      <div className="flex w-full p-4">
+        {/* Search icon inside the search bar */}
+        <div className="flex items-center bg-gray-100 px-4 py-2 mr-8 rounded-full w-full">
+          <SearchIcon className="text-gray-500 mr-4" />
+          <input type="text" placeholder="Search title..." className="bg-transparent focus:outline-none" />
+        </div>
+        {/* Filter icon */}
+
+        <FilterButton />
+      </div>
+
+      {/* print result collection */}
+      <div className='flex-center'>
+        <Collection
+          direction="vertical"
+          itemType='service'
+          items={dummyServices}
+          hasViewMore={true}
+        />
+      </div>
+
+
+
+    </>
   )
 }
 
--- components/shared/FilterButton.tsx
+++ components/shared/FilterButton.tsx
@@ -1,8 +1,33 @@
+"use client"
+
 import React from 'react'
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+import { Modal } from '@mantine/core';
+import { Filter } from '@/public/assets/icons/Filter';
 
 const FilterButton = () => {
+
+  const [opened, { open, close }] = useDisclosure(false);
+  const isMobile = useMediaQuery('(max-width: 50em)');
+
   return (
-    <div>FilterButton</div>
+    <div>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="This is a fullscreen modal"
+        transitionProps={{ transition: 'fade', duration: 200 }}
+        //modal only shows up on bottom 1/2 of the screen using offset
+        xOffset={0}
+        yOffset={350}
+      >
+        The Modal will be full screen only on mobile
+      </Modal>
+      <div className="bg-primary-foreground p-4 rounded-xl">
+        <Filter className="text-white text-xl" onClick={open} />
+      </div> 
+    </div>
+
   )
 }
 
