ff4437db0ae0f548124fdaae7e2c4a615ad50939
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-03-29 02:01:58 -0600

    modal for introduction

--- app/(root)/home.tsx
+++ app/(root)/home.tsx
@@ -9,10 +9,17 @@ import dummyServices from '@/constants/dummyServices';
 import { ServiceItem } from '@/lib/database/models/service.model';
 import Search from '@/components/shared/Search';
 import Link from 'next/link';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal } from '@mantine/core';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
+
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState<string>("Recommendations");
   const [services, setServices] = useState<ServiceItem[]>([]);
+  const [opened, { open, close }] = useDisclosure(true);
 
   useEffect(() => {
     setServices(dummyServices);
@@ -42,6 +49,27 @@ export default function Home() {
 
   return (
     <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        withCloseButton={false}
+        centered
+        size="lg"
+        transitionProps={{ transition: 'fade', duration: 200 }}
+      >
+        <div className='flex flex-col gap-y-4 p-4'>
+          <div className='flex-center'>
+            <div className='h3-bold'>Thank you so much for visiting!</div>
+          </div>
+          <div className='p2-regular'>
+            I am currently in the process of building this site but feel free to browse around and let me know if you have any feedback or suggestions! 🚀
+          </div>
+          <Button onClick={close} className='mx-auto p-10'>
+            <div className='h4-bold'>Browse around</div>
+          </Button>
+        </div>
+      </Modal>
+
       <section className="flex flex-col sticky top-0 bg-white z-50 md:pb-2 border-b border-gray-200">
         <div className='wrapper'>
           <HomeHeader />
@@ -50,7 +78,7 @@ export default function Home() {
         <div className='wrapper pt-4 lg:pt-0 '>
           <div className='flex justify-between md:pb-2'>
             <Link href={"/search"} className='w-full mx-4 '>
-              <Search disabled={true}/>
+              <Search disabled={true} />
             </Link>
           </div>
           <CategoryGroup onCategorySelect={(category: string) => setSelectedCategory(category)} />
@@ -80,4 +108,8 @@ export default function Home() {
       </section>
     </>
   );
+
 }
+
+
+
