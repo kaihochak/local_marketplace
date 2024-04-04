75b0f6fd87874bd36d3a17902c5d69e56e9ca976
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2024-04-04 14:24:49 -0600

    skeleton for dashboard

--- app/(root)/services/[id]/serviceTable.tsx
+++ app/(root)/services/[id]/serviceTable.tsx
@@ -1,42 +1,204 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { ColumnDef, ColumnFiltersState, SortingState, VisibilityState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Input } from "@/components/ui/input"
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Modal } from '@mantine/core';
+import Confetti from 'react-confetti';
+import { useDisclosure } from '@mantine/hooks';
 import { ServiceOffered, columns } from "./columns"
-import { DataTable } from "./data-table"
 
-async function getData(): Promise<ServiceOffered[]> {
-  // Fetch data from API here.
-  return [
-    {
-      id: "728ed52f",
-      service: "Wall Painting",
-      rating: 4.5,
-      comments: 100,
-      availability: "Today",
-      price: 100,
-    },
-    {
-      id: "728ed52f",
-      service: "Premium Wall Painting",
-      rating: 4.8,
-      comments: 20,
-      availability: "Today",
-      price: 300,
-    },
-    {
-      id: "728ed52f",
-      service: "Quick & Dirty",
-      rating: 2.3,
-      comments: 200,
-      availability: "Today",
-      price: 60,
-    },
-  ]
-}
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+};
 
-export default async function ServiceTable() {
-  const data = await getData()
+export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = useState({})
+    const [opened, { open, close }] = useDisclosure(false);
+    const [selectedServices, setSelectedServices] = useState<ServiceOffered[]>([])
 
-  return (
-    <div className="pt-6 pb-3">
-      <DataTable columns={columns} data={data} />
-    </div>
-  )
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        // getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+        },
+    })
+
+    const handleRowSelect = (rowId: string) => {
+        // Get the selected row data
+        const selectedRow = table.getRowModel().rows.find((row) => row.id === rowId);
+        console.log(selectedRow?.original);
+        setSelectedServices([...selectedServices, selectedRow?.original as ServiceOffered]);
+    };
+
+    // for DEBUG
+    useEffect(() => {
+        console.log(selectedServices)
+    }, [selectedServices])
+
+    const confettiProps = typeof window !== 'undefined' ? {
+        width: window.innerWidth,
+        height: window.innerHeight
+    } : {};
+
+    // Confirming reservation
+    const ReserveDashboard = () => {
+
+        console.log(selectedServices);
+        
+        return (
+            <Modal
+                opened={opened}
+                onClose={close}
+                title=""
+                transitionProps={{ transition: 'fade', duration: 200 }}
+            >
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-3xl font-semibold mt-5 text-left">Reservation Details</h1>
+                </div>
+
+                {/* Table */}
+
+                    
+
+
+                {/* show confetti */}
+                {/* <Confetti
+                {...confettiProps}
+                numberOfPieces={500}
+                recycle={false}
+                initialVelocityY={10}
+                initialVelocityX={10}
+                colors={['#f44336', '#2196f3', '#ffeb3b', '#4caf50']}
+            /> */}
+
+            </Modal>
+        )
+    }
+
+    return (
+        <div className="rounded-lg border p-4">
+            <div className="flex items-between px-2 gap-x-6 pt-2 pb-4">
+                {/* Search bar */}
+                <Input
+                    placeholder="Find services..."
+                    value={(table.getColumn("service")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("service")?.setFilterValue(event.target.value)
+                    }
+                    className="w-full"
+                />
+                {/* show */}
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="ml-auto">
+                            Show
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        {table
+                            .getAllColumns()
+                            .filter(
+                                (column) => column.getCanHide()
+                            )
+                            .map((column) => {
+                                return (
+                                    <DropdownMenuCheckboxItem
+                                        key={column.id}
+                                        className="capitalize"
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={(value) =>
+                                            column.toggleVisibility(!!value)
+                                        }
+                                    >
+                                        {column.id}
+                                    </DropdownMenuCheckboxItem>
+                                )
+                            })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+
+            {/* Content */}
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                                onClick={() => handleRowSelect(row.id)}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+
+            {/* Bottom */}
+            <div className="flex items-center justify-end space-x-2 p-2">
+                {/* # of selected service */}
+                <div className="flex-1 text-sm text-muted-foreground">
+                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} service(s) selected.
+                </div>
+
+                {/* Reserve Button */}
+                <Button onClick={open} variant="default">Reserve</Button>
+            </div>
+
+            {/* Reserve Modal */}
+            <ReserveDashboard />
+
+
+        </div>
+    )
 }
