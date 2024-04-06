c092c6a2de1eb4aa736c066e6bc7df1f18e70709
kusumpreetheer <55485344+kusumpreetheer@users.noreply.github.com>
2024-04-06 01:05:52 -0600

    payment

--- app/(root)/services/[id]/serviceTable.tsx
+++ app/(root)/services/[id]/serviceTable.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { FaCreditCard, FaPaypal, FaStripe } from 'react-icons/fa';
 import { useEffect, useRef, useState } from "react"
 import { ColumnDef, ColumnFiltersState, SortingState, VisibilityState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
@@ -21,7 +22,8 @@ export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TD
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
     const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
     const [rowSelection, setRowSelection] = useState({})
-    const [opened, { open, close }] = useDisclosure(false);
+    const [opened, { open: openDisclosure, close: closeDisclosure }] = useDisclosure(false);
+    const [openPaymentModal, { open: openPayment, close: closePayment }] = useDisclosure(false);
     const [selectedServices, setSelectedServices] = useState<ServiceOffered[]>([])
 
     const table = useReactTable({
@@ -66,6 +68,113 @@ export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TD
         height: window.innerHeight
     } : {};
 
+    const PaymentScreen = () => {
+
+        const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<string | null>(null);
+
+        const handleCardPayment = () => {
+            setSelectedPaymentMethod('card');
+        }
+
+        const handleStripePayment = () => {
+            setSelectedPaymentMethod('stripe');
+        }
+
+        const handlePaypalPayment = () => {
+            setSelectedPaymentMethod('paypal');
+        }
+
+        return (
+            <Modal
+                opened={openPaymentModal}
+                onClose={closePayment}
+                title=""
+                transitionProps={{ transition: 'fade', duration: 200 }}
+            >
+                {/* <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-3xl font-semibold my-5 text-left">Payment</h1>
+                </div> */}
+
+                <div className="flex items-center justify-center space-x-4 pb-5">
+                    <div className="flex items-center justify-center w-8 h-8 bg-indigo-500 text-white rounded-full pl-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 transform rotate-180" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                fillRule="evenodd"
+                                d="M16.707 4.293a1 1 0 00-1.414 0l-8 8a1 1 0 001.414 1.414l7.293-7.293 7.293 7.293a1 1 0 001.414-1.414l-8-8z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <div className="w-12 h-0.5 bg-gray-200"></div>
+                    <div className="flex items-center justify-center w-8 h-8 bg-green-200 text-gray-700 rounded-full">2</div>
+                    <div className="w-12 h-0.5 bg-gray-200"></div>
+                    <div className="flex items-center justify-center w-8 h-8 bg-gray-200 text-gray-700 rounded-full">3</div>
+                </div>
+
+                {/* Payment methods section */}
+                <div className="flex flex-col items-center justify-right mb-5">
+                    <h2 className="text-xl font-semibold mb-3">Select Payment Method:</h2>
+                    <div className="flex gap-3 w-full h-10">
+                        <button onClick={handleCardPayment} className="flex items-center justify-center w-full px-4 py-2 border rounded-md shadow-sm bg-gray-200 text-gray-700  hover:bg-white">
+                            <FaCreditCard className="mr-2" />
+                        </button>
+                        <button onClick={handleStripePayment} className="flex items-center justify-center w-full px-2 py-2 border rounded-md shadow-sm bg-gray-200 text-gray-700  hover:bg-white">
+                            <FaStripe className="mr-2" />
+                        </button>
+                        <button onClick={handlePaypalPayment} className="flex items-center justify-center w-full px-4 py-2 border rounded-md shadow-sm bg-gray-200 text-gray-700  hover:bg-white">
+                            <FaPaypal className="mr-2" />
+                        </button>
+                    </div>
+                </div>
+
+
+                {/* Payment method specific sections */}
+                {selectedPaymentMethod === 'card' && (
+                    <div className="flex flex-col items-center justify-center">
+                        {/* Add card payment specific content here */}
+                        <form className="w-full max-w-sm">
+                            <div className="mb-4">
+                                <label htmlFor="cardName" className="block text-gray-700 font-semibold mb-2">Cardholder's Name</label>
+                                <input type="text" id="cardName" name="cardName" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500" placeholder="Enter cardholder's name" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="cardNumber" className="block text-gray-700 font-semibold mb-2">Card Number</label>
+                                <input type="text" id="cardNumber" name="cardNumber" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500" placeholder="Enter card number" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="expiryDate" className="block text-gray-700 font-semibold mb-2">Expiry Date</label>
+                                <input type="text" id="expiryDate" name="expiryDate" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500" placeholder="MM/YY" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="cvv" className="block text-gray-700 font-semibold mb-2">CVV</label>
+                                <input type="text" id="cvv" name="cvv" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500" placeholder="Enter CVV" />
+                            </div>
+                            <div className="flex justify-center">
+                                <button type="submit" className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Pay Now</button>
+                            </div>
+                        </form>
+                    </div>
+                )}
+
+                {selectedPaymentMethod === 'stripe' && (
+                    <div className="flex flex-col items-center justify-center">
+                        {/* Add Stripe payment specific content here */}
+                        <h2 className="text-lg font-semibold mb-3">Stripe Payment Section</h2>
+                        <p>Use Stripe payment gateway here...</p>
+                    </div>
+                )}
+
+                {selectedPaymentMethod === 'paypal' && (
+                    <div className="flex flex-col items-center justify-center">
+                        {/* Add Paypal payment specific content here */}
+                        <h2 className="text-lg font-semibold mb-3">Paypal Payment Section</h2>
+                        <p>Use Paypal payment gateway here...</p>
+                    </div>
+                )}
+            </Modal>
+        )
+    }
+
     // Confirming reservation
     const ReserveDashboard = () => {
 
@@ -87,13 +196,22 @@ export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TD
             calculateTotalPrice();
         }, [selectedServices]);
 
+
         return (
             <Modal
                 opened={opened}
-                onClose={close}
+                onClose={closeDisclosure}
                 title=""
                 transitionProps={{ transition: 'fade', duration: 200 }}
             >
+                <div className="flex items-center justify-center space-x-4 pb-5">
+                <div className="flex items-center justify-center w-8 h-8 bg-green-200 text-gray-700 rounded-full">1</div>
+                    <div className="w-12 h-0.5 bg-gray-200"></div>
+                    <div className="flex items-center justify-center w-8 h-8 bg-green-200 text-gray-700 rounded-full">2</div>
+                    <div className="w-12 h-0.5 bg-gray-200"></div>
+                    <div className="flex items-center justify-center w-8 h-8 bg-gray-200 text-gray-700 rounded-full">3</div>
+                </div>
+
                 <div className="flex flex-col items-center justify-center">
                     <h1 className="text-3xl font-semibold my-5 text-left">Reservation Details</h1>
                 </div>
@@ -129,8 +247,10 @@ export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TD
 
                 {/* okay button */}
                 <div className="flex justify-end">
-                    <Button onClick={close} variant="default">Okay</Button>
+                    <Button onClick={openPayment} variant="default">Okay</Button>
                 </div>
+
+                <PaymentScreen />
 
                 {/* show confetti */}
                 {/* <Confetti
@@ -243,7 +363,7 @@ export function ServiceTable<TData, TValue>({ columns, data }: DataTableProps<TD
                 </div>
 
                 {/* Reserve Button */}
-                <Button onClick={open} variant="default">Reserve</Button>
+                <Button onClick={openDisclosure} variant="default">Reserve</Button>
             </div>
 
             {/* Reserve Modal */}
